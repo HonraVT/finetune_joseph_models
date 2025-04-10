de10d49c0c728c3309a1f2c1eb56641ee0d5619d
HonraVT <111161607+HonraVT@users.noreply.github.com>
2025-04-10 11:41:17 -0300

    Update training_on_modal.py

--- training_on_modal.py
+++ training_on_modal.py
@@ -64,7 +64,6 @@ def run_trainer():
     #######
 
     from unsloth import FastLanguageModel
-    import torch
 
     max_seq_length = 2048  # Choose any! We auto support RoPE Scaling internally!
     dtype = None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
